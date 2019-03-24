12a63517af61ad346d6203a3f8eb6996cba116d1
Evan Cole <18554853+colevandersWands@users.noreply.github.com>
2019-03-24 17:30:46 +0100

    Update init-from-permalink.js

--- utils/permalinking/init-from-permalink.js
+++ utils/permalinking/init-from-permalink.js
@@ -2,6 +2,7 @@ var query_challenge = read_challenge_query();
 
 if (query_challenge) {
   // current_challenge = decode_challenge(query_challenge); // encoded uri's are automatically decoded
+   current_challenge = query_challenge;
   load_challenge(current_challenge);
 };
 
