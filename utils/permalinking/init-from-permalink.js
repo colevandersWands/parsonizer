5cc10b76bd095a233812b983b85f5872611bbcc0
colevandersWands <18554853+colevandersWands@users.noreply.github.com>
2019-03-24 01:51:07 +0100

    uri-de-decoding

--- utils/permalinking/init-from-permalink.js
+++ utils/permalinking/init-from-permalink.js
@@ -1,7 +1,7 @@
 var query_challenge = read_challenge_query();
 
 if (query_challenge) {
-  current_challenge = decode_challenge(query_challenge);
+  // current_challenge = decode_challenge(query_challenge); // encoded uri's are automatically decoded
   load_challenge(current_challenge);
 };
 
